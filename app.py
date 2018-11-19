d101ad3dd7456c3962e22ecb493000344f5990a0
Arun Abraham <arun.abraham@wsp11154.local>
2018-11-19 01:11:06 +0100

    city default to barcelona

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
+    city = "Barcelona"
     if city is None:
         return None
 
