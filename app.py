e8487c14087be690639aad817aacfd3f0c5749df
Arun Abraham <arun.abraham@wsp11154.local>
2018-11-22 10:46:40 +0100

    added unirest and handling of url summaries: fixed if

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def prawProcessUrl (url):
 def processUrl(req):
         url = req.get("result").get("parameters").get("url")
 
-        if "reddit.com" in url
+        if url.find("reddit.com):
             result =  prawProcessUrl(url)
             print("Praw complete")
             print(result)
