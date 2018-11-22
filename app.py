a556f699b3342c8638e34f1386c312908e3e668e
Arun Abraham <arun.abraham@wsp11154.local>
2018-11-22 22:32:21 +0100

    debugging actual url

--- app.py
+++ app.py
@@ -117,9 +117,10 @@ def processUrl(req):
             #"Accept": "application/json"
          # },
 
-#          params=("{\"url\":" + re.sub("/", "\/"  , url) + ",\"text\":\"\",\"sentnum\":8}")
+         #          params=("{\"url\":" + re.sub("/", "\/"  , url) + ",\"text\":\"\",\"sentnum\":8}")
         #)
-# re.escape is to add backslash before all special charaters
+        # re.escape is to add backslash before all special charaters
+        print("new")
         print("passing" + re.sub("/", "\/"  , url))
 
         print("unirest complete")
