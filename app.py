9bcfc02579ab23b7c6c1e2cc92c16a2ec5cda9bb
Arun Abraham <arun.abraham@wsp11154.local>
2018-11-22 17:14:46 +0100

    debug text summarizer after purchasing

--- app.py
+++ app.py
@@ -109,13 +109,14 @@ def processUrl(req):
         return res
     else:
         print("Inside non-reddit, doing unirest")
+        # These code snippets use an open-source library. http://unirest.io/python
         response = unirest.post("https://textanalysis-text-summarization.p.mashape.com/text-summarizer",
           headers={
             "X-Mashape-Key": "oz0JfIM2BVmshrK6jybJa9VO9Lvkp1jVTJdjsnsqzjJv1QwnxA",
             "Content-Type": "application/json",
             "Accept": "application/json"
           },
-          params=("{\"url\":url,\"text\":\"\",\"sentnum\":8}")
+          params=("{\"url\":\"http://en.wikipedia.org/wiki/Automatic_summarization\",\"text\":\"\",\"sentnum\":8}")
         )
         res = response.body
         print(response.body)
