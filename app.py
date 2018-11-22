4db260141e64e53ab668e87a7697b643b78acd5d
Arun Abraham <arun.abraham@wsp11154.local>
2018-11-22 23:14:44 +0100

    debugging actual url

--- app.py
+++ app.py
@@ -110,8 +110,16 @@ def processUrl(req):
         return res
     else:
         print("Inside non-reddit, doing unirest")
+
         # These code snippets use an open-source library. http://unirest.io/python
-        print("{\"url\":" + re.sub("/", "\/"  , url) + ",\"text\":\"\",\"sentnum\":8}")
+        response = unirest.post("https://textanalysis-text-summarization.p.mashape.com/text-summarizer",
+          headers={
+            "X-Mashape-Key": "oz0JfIM2BVmshrK6jybJa9VO9Lvkp1jVTJdjsnsqzjJv1QwnxA",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          params=("{\"url\":" + re.sub("/", "\/"  , url) + ",\"text\":\"\",\"sentnum\":8}")
+        )
 
         print("unirest complete")
         sentence = response.body.get("sentences")
